c9b0b5ca167a88d17c1a9563501b5a6f81fdcd68
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2026-07-22 16:31:19 +0700

    Add kitchen scriptable object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact() {
         Debug.Log("Interact");
+        var kitchenObjectSpawn = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectSpawn.transform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectSO.objectName);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/KitchenObjectSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class KitchenObjectSO : ScriptableObject
+{
+    public GameObject prefab;
+
+    public Sprite sprite;
+
+    public string objectName;
+}
