c09d7e3da23fd091d4b9c05930060c10bf73ce8d
Nguyen Quoc Tin <tin.9a1.dt@gmail.com>
2026-08-02 17:04:40 +0700

    pick/drop interaction

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,6 +5,14 @@ public class ClearCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-       
+        // Player drop object on counter
+        if (player.GetKitchenObject() != null && GetKitchenObject() == null) {
+            player.GetKitchenObject().SetKitchenObjectParent(this);
+        }
+        // Player pick up object from counter
+        else if (player.GetKitchenObject() == null && GetKitchenObject() != null) {
+            GetKitchenObject().SetKitchenObjectParent(player);
+        }
+        
     }
 }
